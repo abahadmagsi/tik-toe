c6491d052129afe4dc2d682e606d90ade331ad78
ahadmagsi <abdulahadmagsi1@gmail.com>
2023-12-10 22:53:24 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+// /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  /*----- app's state (variables) -----*/
+  let board;
+  let turn = 'X';
+  let win;
+  
+  /*----- cached element references -----*/
+  const squares = Array.from(document.querySelectorAll('#board div'));
+  
+  /*----- event listeners -----*/
+  document.getElementById('board').addEventListener('click', handleTurn);
+  const messages = document.querySelector('h2');
+  document.getElementById('reset-button').addEventListener('click', init);
+  /*----- functions -----*/
+  
+  function getWinner() {
+    let winner = null;
+    winningCombos.forEach((combo, index) => {
+      if (
+        board[combo[0]] &&
+        board[combo[0]] === board[combo[1]] &&
+        board[combo[0]] === board[combo[2]]
+      ) {
+        winner = board[combo[0]];
+      }
+    });
+    // new code below
+    if (winner) {
+      return winner;
+    } else if (board.includes('')) {
+      return null; // if there's an empty space, return null (no winner yet)
+    } else {
+      return 'T'; // no winner and no empty spaces? That's a tie!
+    }
+  }
+  
+  function handleTurn(event) {
+    let idx = squares.findIndex(function (square) {
+      return square === event.target;
+    });
+    // new code below
+    if (board[idx] === '' && !win) {
+      board[idx] = turn;
+      turn = turn === 'X' ? 'O' : 'X';
+      win = getWinner();
+      render();
+    }
+  }
+  
+  function init() {
+    board = ['', '', '', '', '', '', '', '', ''];
+    // new code here
+    win = null;
+    render();
+  }
+  
+  function render() {
+    board.forEach(function (mark, index) {
+      //this sets the text content of the square of the same position to the mark on the board.
+      squares[index].textContent = mark;
+    });
+    // new code below
+    if (win) {
+      messages.textContent = `Player ${win} wins!`;
+    } else if (board.includes('')) {
+      messages.textContent = `It's ${turn}'s turn!`;
+    } else {
+      messages.textContent = 'It\'s a tie!';
+    }
+  }
+  init();
+  
